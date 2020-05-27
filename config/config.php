15f41bb56e1f14f44ddcff3cd129c60d95ee2de4
Jeremy Kavuncuoglu <jeremy@skaraudio.com>
2020-05-27 06:33:22 -0400

    Testing config added, Testcase added

--- config/config.php
+++ config/config.php
@@ -9,10 +9,25 @@ return [
      |
      | Shipstation Requires an api key and secret for authentication.
      | You can provide the api_key and api_secret by adding SHIPSTATION_API_KEY
+     | and SHIPSTATION_API_SECRET to your .env file
+     |
+     | Shipstation Testing is by default disabled, to enable it add the
+     | SHIPSTATION_TESTING key to your .env file and set it to true
+     */
+
+     'testing' => env('SHIPSATION_TESTING', false),
+
+    /*
+     |--------------------------------------------------------------------------
+     | API KEY & API SECRET
+     |--------------------------------------------------------------------------
+     |
+     | Shipstation Requires an api key and secret for authentication.
+     | You can provide the api_key and api_secret by adding SHIPSTATION_API_KEY
      | and SHIPSTATION_API_SECRET to your .env fle
      |
      */
 
     'api_key' => env('SHIPSTATION_API_KEY'),
-    'api_secret' => env('SHIPSTATION_API_SECRET')
+    'api_secret' => env('SHIPSTATION_API_SECRET'),
 ];
